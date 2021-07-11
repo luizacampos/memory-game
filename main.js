28a68e40ab701662fed88b4abe4b3fa6eeddb88b
Luiza <luizafcampos@gmail.com>
2021-07-11 23:01:35 +0200

    Add CSS

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-import './style.css'
+import "./src/styles/settings/colors.css";
+import "./src/styles/generic/reset.css";
+import "./src/styles/elements/base.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import CardGame from "./src/components/CardGame";
+
+const $root = document.querySelector("#root");
+
+const $htmlCardGame = CardGame();
+console.log($htmlCardGame);
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
